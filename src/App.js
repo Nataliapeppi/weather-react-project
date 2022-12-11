ba8eb4a37ff41bfe60e05fe95b00b1deac951346
Natalia <ntl.peppi@gmail.com>
2022-12-11 19:55:43 +0100

    footer added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,19 @@
 import "./App.css";
 export default function App() {
-  return <div>Hello from weather app</div>;
+  return (
+    <div>
+      Hello from weather app
+      <footer>
+        This project is coded by Natalia Peppi and it is
+        <a
+          href="https://github.com/Nataliapeppi/weather-react-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          opened-sourced
+        </a>{" "}
+      </footer>
+    </div>
+  );
 }
