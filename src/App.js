4f70196d3186100188614fa8fc1b9dfff553b002
Natalia <ntl.peppi@gmail.com>
2022-12-11 20:17:37 +0100

    bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
+import React from "react";
 import "./App.css";
 export default function App() {
   return (
-    <div>
-      Hello from weather app
-      <footer>
-        This project is coded by Natalia Peppi and it is
-        <a
-          href="https://github.com/Nataliapeppi/weather-react-project"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          opened-sourced
-        </a>{" "}
-      </footer>
+    <div className="App">
+      <div className="container">
+        Hello from weather app
+        <footer>
+          This project is coded by Natalia Peppi and it is
+          <a
+            href="https://github.com/Nataliapeppi/weather-react-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            opened-sourced
+          </a>{" "}
+        </footer>
+      </div>
     </div>
   );
 }
