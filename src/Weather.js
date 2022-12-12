684e6836f5a05885c74dd7709a32212fa0efdbc2
Natalia <ntl.peppi@gmail.com>
2022-12-12 22:58:54 +0100

    basis css added

--- src/Weather.js
+++ src/Weather.js
@@ -11,10 +11,15 @@ export default function Weather() {
               type="search"
               placeholder="Type a city"
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
@@ -23,13 +28,17 @@ export default function Weather() {
         <li>Friday 18:31</li>
         <li>Clear sky</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
-          <img
-            src="http://openweathermap.org/img/wn/01d@2x.png"
-            alt="Clear sky"
-          />
-          7 °C
+          <div className="d-flex">
+            <img
+              src="http://openweathermap.org/img/wn/01d@2x.png"
+              alt="Clear sky"
+              className="float-left"
+            />
+            <span className="temperature float-left">7</span>
+            <span className="unit">°C</span>
+          </div>
         </div>
         <div className="col-6">
           <ul>
